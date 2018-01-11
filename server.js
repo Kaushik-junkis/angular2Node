f9f293f624b0fb61ab6f8b1e9db99367e8c7d97c
Kaushik-junkis <35091642+Kaushik-junkis@users.noreply.github.com>
2018-01-11 11:55:20 +0530

    Comment added
    add comment

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const bodyParser = require('body-parser');
 const api = require('./server/routes/api');
 
 const app = express();
-
+//for browser
 // Parsers for POST data
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
